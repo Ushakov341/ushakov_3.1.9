117fd9bbf2cf180f14f6b1d5dd5a39ce72a04db1
Ushakov341 <git@stackblitz.com>
2025-11-05 22:34:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState, ChangeEvent, memo, useMemo } from 'react';
+import { generateProducts, filterProducts } from './utils.ts';
+import { ProductList } from './ProductList';
+import './App.css';
+
+export const App = memo(() => {
+  const [filterTerm, setFilterTerm] = useState('');
+
+  const dummyProducts = useMemo(() => generateProducts(), []);
+  const filteredProducts = filterProducts(filterTerm, dummyProducts);
+
+  const updateFilterHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setFilterTerm(event.target.value);
+  };
+
+  return (
+    <>
+      <input className="input" type="text" onChange={updateFilterHandler} />
+      <ProductList products={filteredProducts} />
+    </>
+  );
+});
