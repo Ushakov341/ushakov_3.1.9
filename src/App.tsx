07ec77b5ab9c6cf8fe71f880a459ebc2396b93b4
Kirill <111366727+Ushakov341@users.noreply.github.com>
2025-11-06 22:09:03 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState, ChangeEvent, memo, useMemo } from 'react';
+import { useState, ChangeEvent, memo, useMemo, useDeferredValue } from 'react';
 import { generateProducts, filterProducts } from './utils.ts';
 import { ProductList } from './ProductList';
 import './App.css';
 
 export const App = memo(() => {
   const [filterTerm, setFilterTerm] = useState('');
+  const deferredFilterTerm = useDeferredValue(filterTerm);
 
   const dummyProducts = useMemo(() => generateProducts(), []);
-  const filteredProducts = filterProducts(filterTerm, dummyProducts);
+  const filteredProducts = filterProducts(deferredFilterTerm, dummyProducts);
 
   const updateFilterHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setFilterTerm(event.target.value);
@@ -15,7 +16,7 @@ export const App = memo(() => {
 
   return (
     <>
-      <input className="input" type="text" onChange={updateFilterHandler} />
+      <input className="input" type="text" onChange={updateFilterHandler} value={filterTerm} />
       <ProductList products={filteredProducts} />
     </>
   );
